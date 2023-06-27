f8d75f76f78ae80f8e88978e026b8b68a3b1da96
JeallyRabbit <119430599+JeallyRabbit@users.noreply.github.com>
2023-06-27 12:05:04 +0200

    .
    .

--- Computer_Info1/Tools.cs
+++ Computer_Info1/Tools.cs
@@ -202,6 +202,7 @@ namespace Computer_Info1
 
         public static bool setLocalHostName(string newName)
         {
+            /*
             RegistryKey key = Registry.LocalMachine;
 
             string activeComputerName = "SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ActiveComputerName";
@@ -217,6 +218,23 @@ namespace Computer_Info1
             hostName.SetValue("Hostname", newName);
             hostName.SetValue("NV Hostname", newName);
             hostName.Close();
+            */
+            ProcessStartInfo process = new ProcessStartInfo();
+
+            // set name of process to "WMIC.exe"
+            process.FileName = "WMIC.exe";
+
+            // pass rename PC command as argument
+            process.Arguments = "computersystem where caption='" + System.Environment.MachineName + "' rename " + newName;
+
+            // Run the external process & wait for it to finish
+            using (Process proc = Process.Start(process))
+            {
+                proc.WaitForExit();
+
+                // print the status of command
+                Console.WriteLine("Exit code = " + proc.ExitCode);
+            }
             return true;
         }
 
